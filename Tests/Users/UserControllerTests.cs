238dd9aabb80353fd761863f732d6d871cb49761
navff <var@33kita.ru>
2021-03-26 07:42:12 +0300

    Add 401 user test

--- Tests/Users/UserControllerTests.cs
+++ Tests/Users/UserControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Net;
+using System.Net.Http.Headers;
 using web.api.App.Users;
 using web.api.App.Users.Commands;
 using Xunit;
@@ -24,6 +25,14 @@ namespace Tests.Users
             var response = await _httpClient.GetAsync("user/me");
             var result = await response.Content.ReadAsStringAsync();
             Assert.Equal("var@33kita.ru", result);
+        }
+
+        [Fact]
+        public async void GetUser_401_Test()
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "jopa");
+            var response = await _httpClient.GetAsync("user/me");
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
     }
 }
